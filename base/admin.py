13a737d36d8ce2e213cd699b60270fc783444561
Kharay33455 <shakibkhan276337@gmail.com>
2024-10-03 18:19:23 +0100

    Admin Panel designed

--- base/admin.py
+++ base/admin.py
@@ -2,17 +2,36 @@ from django.contrib import admin
 from .models import *
 
 
+class EmailAdmin(admin.ModelAdmin):
+    readonly_fields = ('email',)
+
+    def has_add_permission(self, request):
+        return False
+    
+    def has_delete_permission(self, request, obj = None):
+        return False
+    
+class MessageAdmin(admin.ModelAdmin):
+    readonly_fields = ('email', 'title', 'message',)
+
+    def has_add_permission(self, request):
+        return False
+    
+    def has_delete_permission(self, request, obj = None):
+        return False
+
+
+
 admin.site.register(PrayerTime)
 admin.site.register(Event)
 admin.site.register(Address)
 admin.site.register(Service)
 admin.site.register(Photo)
-admin.site.register(Message)
-admin.site.register(Mosque)
+admin.site.register(Message, MessageAdmin)
 admin.site.register(MosqueName)
 admin.site.register(Leader)
 admin.site.register(Download)
-admin.site.register(Email)
+admin.site.register(Email, EmailAdmin)
 admin.site.register(BankAccount)
 
 # Register your models here.
